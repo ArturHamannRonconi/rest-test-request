12cbeca4d2105beb8d8930aab31c9b9ec2413c0b
ArturHamannRonconi <arturhronconi@gmail.com>
2021-08-21 19:56:17 -0300

    finish request

--- src/main/java/com/artur/Main.java
+++ src/main/java/com/artur/Main.java
@@ -1,15 +1,18 @@
 package com.artur;
 
+import java.util.UUID;
+
 public class Main 
 {
     public static void main( String[] args )
     {
         var app = new App();
+        var uuid = UUID.randomUUID();
+
+        var responseGET = app.get("http://localhost:8080/user/" + uuid);
         
-        var responseGET = app.get("http://localhost:3000/api/v1/get");
-        var bodyJson = "{ \"response\": " + responseGET.body() + ", \"response2\": \"Hello World Part 2!\" }";
-        
-        var responsePOST = app.post("http://localhost:3000/api/v1/post/body", bodyJson);
+        var userData = "{\"id\":\""+ uuid +"\", \"email\":\"user@mail.com\", \"password\":\"123456\"}";
+        var responsePOST = app.post("http://localhost:8080/user", userData);
 
         System.out.println(responseGET.body());
         System.out.println(responsePOST.body());
